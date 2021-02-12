f7374b535f67b6b8b8d55a3a8da99199fea342ee
Marcin Kolenda <210036@edu.p.lodz.pl>
2021-02-12 10:42:11 +0100

    Ola: Porównanie Bayes K2, maxParents = 5 z maxParents = 2

--- src/test/java/projekt/essaerae/AnalizaGlass.java
+++ src/test/java/projekt/essaerae/AnalizaGlass.java
@@ -8,11 +8,82 @@ public class AnalizaGlass {
 	public static void main(String[] args) throws Exception {
 		Instances dane = DataSource.read("zasoby/weka-data/glass.arff");
 		dane.setClassIndex(9);
+		wypiszDane(dane);
+
+		wykonajSiecBK2(dane, 2);
+//		Bayes Network Classifier
+//		not using ADTree
+//		#attributes=10 #classindex=9
+//		Network structure (nodes followed by parents)
+//		RI(3): Type
+//		Na(2): Type
+//		Mg(2): Type RI
+//		Al(3): Type RI
+//		Si(1): Type
+//		K(4): Type Mg
+//		Ca(4): Type Mg
+//		Ba(2): Type
+//		Fe(1): Type
+//		Type(7):
+//		LogScore Bayes: -1154.1645405405316
+//		LogScore BDeu: -1772.6151459308992
+//		LogScore MDL: -1691.4074842654582
+//		LogScore ENTROPY: -1205.7866549059813
+//		LogScore AIC: -1386.7866549059809
+
+		wykonajSiecBK2(dane, 5);
+//		Bayes Network Classifier
+//		not using ADTree
+//      #attributes=10 #classindex=9
+//		Network structure (nodes followed by parents)
+//		RI(3): Type
+//		Na(2): Type
+//		Mg(2): Type RI
+//		Al(3): Type RI
+//		Si(1): Type
+//		K(4): Type Mg Na
+//		Ca(4): Type Mg RI
+//		Ba(2): Type
+//		Fe(1): Type
+//		Type(7):
+//		LogScore Bayes: -1149.6668973758815
+//		LogScore BDeu: -2434.0768119165823
+//		LogScore MDL: -2130.703472045588
+//		LogScore ENTROPY: -1307.0261537397337
+//		LogScore AIC: -1614.0261537397332
+
+
+
+//		wykonajSiecBHC(dane, 2);
+//		wykonajSiecBHCReverse(dane, 2);
+
+		// multilayer perceptron
+		// svm
+
+		// cross-validacje z macierzami pomyłek
+	}
+
+	private static void wypiszDane(Instances dane) {
 		System.out.println(dane);
-		SiecB bn = SiecB.createK2(2);
+	}
+
+	private static void wykonajSiecBHCReverse(Instances dane, int maxNrOfParents) throws Exception {
+		SiecB bn = SiecB.createHillClimbGlobal(maxNrOfParents, true);
 		bn.buildClassifier(dane);
 		System.out.println(bn);
-		
+	}
+
+	private static void wykonajSiecBHC(Instances dane, int maxNrOfParents) throws Exception {
+		SiecB bn = SiecB.createHillClimbGlobal(maxNrOfParents);
+		bn.buildClassifier(dane);
+		System.out.println(bn);
+	}
+
+	private static void wykonajSiecBK2(Instances dane, int maxNrOfParents) throws Exception {
+		// sieć bayesowska k2
+		SiecB bn = SiecB.createK2(maxNrOfParents);
+		bn.buildClassifier(dane);
+		System.out.println(bn);
 	}
 
 }
